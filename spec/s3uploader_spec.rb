4e0a3cf1c7b7d35639bc14d51a0d86da4a8f9158
Christian Hein <chrishein@gmail.com>
2012-09-14 11:21:16 -0300

    Mock fog and generate random files for test

--- spec/s3uploader_spec.rb
+++ spec/s3uploader_spec.rb
@@ -1,9 +1,30 @@
 require 'spec_helper'
 
 describe S3Uploader do  
+    before :all do
+      Fog.mock!
+      @base_test_directory = '/tmp/test_s3_uploader'
+      create_test_files(@base_test_directory, 10)
+      create_test_files("#{@base_test_directory}/subdir1", 5)
+    end
     
     it "should upload all files in a directory" do
-      pending 'Write real test'
-      S3Uploader.upload_directory('/tmp/test', 'mybucket', { :destination_dir => 'test/' })
+      S3Uploader.upload_directory(@base_test_directory, 'mybucket', { :destination_dir => 'test1/' })
     end
+    
+    after :all do
+      FileUtils.rm @base_test_directory
+    end
+end
+
+def create_test_files(directory, number_of_files)
+  FileUtils.mkdir_p directory
+  
+  $stdout = File.new( '/dev/null', 'w' )
+
+  number_of_files.times do |i|
+    `dd if=/dev/zero of=#{directory}/file#{i}.txt count=1024 bs=1024`
+  end
+  
+  $stdout = STDOUT  
 end
