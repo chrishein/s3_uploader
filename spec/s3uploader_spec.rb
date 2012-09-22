ba1788e77ac3b58125357c132564e5d5d342ca0f
Christian Hein <chrishein@gmail.com>
2012-09-22 00:10:15 -0300

    Test directory upload

--- spec/s3uploader_spec.rb
+++ spec/s3uploader_spec.rb
@@ -3,28 +3,42 @@ require 'spec_helper'
 describe S3Uploader do  
     before :all do
       Fog.mock!
-      @base_test_directory = '/tmp/test_s3_uploader'
-      create_test_files(@base_test_directory, 10)
-      create_test_files(File.join(@base_test_directory, 'subdir1'), 5)
+      
+      @connection = Fog::Storage.new({
+        :provider                 => 'AWS',
+        :aws_access_key_id        => '11111111111',
+        :aws_secret_access_key    => 'XXXXXXXXXXXXXXXXXXXXXXXXXXX'
+      })
+      
+      @connection.directories.create(
+        :key    => 'mybucket',
+        :public => true
+      )
+      
+      @tmp_directory = File.join(Dir.tmpdir, 'test_s3_uploader')
+      create_test_files(@tmp_directory, 10)
+      create_test_files(File.join(@tmp_directory, 'subdir1'), 5)
+      @logger = Logger.new(StringIO.new)
     end
     
     it "should upload all files in a directory" do
-      S3Uploader.upload_directory(@base_test_directory, 'mybucket', { :destination_dir => 'test1/' })
+      
+      @logger.should_receive(:info).exactly(15).times.with(/Uploading/)
+      
+      S3Uploader.upload_directory(@tmp_directory, 'mybucket',
+        { :destination_dir => 'test1/',
+          :logger => @logger,
+          :connection => @connection
+        })
+      
     end
     
-    after :all do
-      FileUtils.rm @base_test_directory
-    end
 end
 
 def create_test_files(directory, number_of_files)
   FileUtils.mkdir_p directory
-  
-  $stdout = File.new( '/dev/null', 'w' )
 
   number_of_files.times do |i|
-    `dd if=/dev/zero of=#{directory}/file#{i}.txt count=1024 bs=1024`
-  end
-  
-  $stdout = STDOUT  
+    Open3.popen3('dd if=/dev/zero of=#{directory}/file#{i}.txt count=1024 bs=1024')
+  end  
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,8 @@
 require 'rspec'
 require 's3_uploader'
+require 'tmpdir'
+require 'fog'
+require 'open3'
 
 RSpec.configure do |config|
   config.color_enabled = true
