f08166754ddac58d442ff933c14c97451ef4f522
Brandon Hilkert <brandonhilkert@gmail.com>
2013-11-06 10:22:18 -0500

    Add cache-control headers

--- lib/s3_uploader/s3_uploader.rb
+++ lib/s3_uploader/s3_uploader.rb
@@ -9,16 +9,16 @@ module S3Uploader
       :public => false,
       :region => 'us-east-1'
     }.merge(options)
-    
+
     log = options[:logger] || Logger.new(STDOUT)
-    
+
     raise 'Source must be a directory' unless File.directory?(source)
-    
+
     if options[:connection]
       connection = options[:connection]
     else
       raise "Missing access keys" if options[:s3_key].nil? or options[:s3_secret].nil?
-      
+
       connection = Fog::Storage.new({
           :provider => 'AWS',
           :aws_access_key_id => options[:s3_key],
@@ -26,7 +26,7 @@ module S3Uploader
           :region => options[:region]
       })
     end
-    
+
     source = source.chop if source.end_with?('/')
     if options[:destination_dir] != '' and !options[:destination_dir].end_with?('/')
       options[:destination_dir] = "#{options[:destination_dir]}/"
@@ -36,20 +36,20 @@ module S3Uploader
     Dir.glob("#{source}/**/*").select{ |f| !File.directory?(f) }.each do |f|
       files << f
       total_size += File.size(f)
-      
+
     end
-    
+
     directory = connection.directories.new(:key => bucket)
-    
+
     start = Time.now
     total_files = files.size
     file_number = 0
     @mutex = Mutex.new
-    
+
     threads = []
     options[:threads].times do |i|
       threads[i] = Thread.new {
-        
+
         until files.empty?
           @mutex.synchronize do
             file_number += 1
@@ -60,22 +60,23 @@ module S3Uploader
             key = file.gsub(source, '')[1..-1]
             dest = "#{options[:destination_dir]}#{key}"
             log.info("[#{Thread.current["file_number"]}/#{total_files}] Uploading #{key} to s3://#{bucket}/#{dest}")
-          
+
             directory.files.create(
               :key    => dest,
               :body   => File.open(file),
-              :public => options[:public]
+              :public => options[:public],
+              :metadata => { "Cache-Control" => "max-age=#{60 * 60 * 24 * 30 * 12}" }
             )
           end
-        end 
+        end
       }
     end
     threads.each { |t| t.join }
-    
+
     finish = Time.now
     elapsed = finish.to_f - start.to_f
     mins, secs = elapsed.divmod 60.0
     log.info("Uploaded %d (%.#{0}f KB) in %d:%04.2f" % [total_files, total_size / KILO_SIZE, mins.to_i, secs])
-    
+
   end
 end
